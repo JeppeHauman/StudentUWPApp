eb475f35fee5a22b3451154481217f49a87f6c2c
Jeppe Hauman <jeppe_hauman@hotmail.com>
2020-04-22 10:50:29 +0200

    Added INotifyPropertyChanged
    Added interface and method using it.
    Modified Navn prop

---
+++ StudentUWPApp/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentUWPApp
+{
+    public class Student : INotifyPropertyChanged
+	{
+		private string _navn;
+
+		public string Navn
+		{
+			get { return _navn; }
+			set 
+				{
+					if (_navn != value)
+					{
+						_navn = value;
+						this.OnPropertyChanged();
+					}
+				}
+			}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged([CallerMemberName] string Navn = null) 
+			=> this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Navn));
+	}
+}
